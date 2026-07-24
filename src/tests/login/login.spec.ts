43a6c20da6422ba50146c819f1ef59e12a69d496
Satish Kacharla <kacharlasatish1@gmail.com>
2026-07-24 20:57:06 +0530

    Update login.spec.ts

--- src/tests/login/login.spec.ts
+++ src/tests/login/login.spec.ts
@@ -14,7 +14,7 @@ test.describe('NC - Login Scenarios', () => {
     ]
 
     // users.forEach(({ role, user }) => {
-    test.only(`Verify Login Success for ${Users.admin.email}`, async ({ header }) => {
+    test(`Verify Login Success for ${Users.admin.email}`, async ({ header }) => {
         await header.navigate();
         const loginPage = await header.openLoginPage(); 
         const homePage = await loginPage.login(Users.admin);
