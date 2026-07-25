9bc68f4f149d95ba75457c7dc5db26d53f5a8822
Satish Kacharla <kacharlasatish1@gmail.com>
2026-07-25 10:57:50 +0530

    Update Env configuration

--- src/config/Env.ts
+++ src/config/Env.ts
@@ -1,26 +1,30 @@
-import dotenv from 'dotenv';
+import fs from "fs";
+import dotenv from "dotenv";
 
 const testEnv = process.env.TEST_ENV || "qa";
+const envFile = `.env.${testEnv}`;
 
 console.log("Before loading");
 console.log("TEST_ENV =", process.env.TEST_ENV);
-console.log("ENV =", process.env.ENV);
 
-const result = dotenv.config({
-    path: `.env.${testEnv}`,
-    override: true
-})
+if (fs.existsSync(envFile)) {
+    console.log(`Loading ${envFile}`);
 
-if (result.error) {
-    throw result.error;
+    dotenv.config({
+        path: envFile,
+        override: false,
+    });
+} else {
+    console.log(`${envFile} not found. Using Jenkins environment variables.`);
 }
 
 console.log("After loading");
-console.log("TEST_ENV =", process.env.TEST_ENV);
-console.log("ENV =", process.env.ENV);
+console.log("BASE_URL =", process.env.BASE_URL);
 
 export class Env {
+
     static readonly environment = testEnv;
+
     static readonly baseUrl = process.env.BASE_URL!;
 
     static readonly adminEmail = process.env.ADMIN_EMAIL!;
@@ -28,5 +32,4 @@ export class Env {
 
     static readonly customerEmail = process.env.CUSTOMER_EMAIL!;
     static readonly customerPassword = process.env.CUSTOMER_PASSWORD!;
-
 }
